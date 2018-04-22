ad9603795f5dfd65c897ca22aca59afa8a178198
Peter Sarkis <peter@peters-mbp.attlocal.net>
2018-04-21 21:24:42 -0500

    first test version

---
+++ js/app.js
@@ -0,0 +1,107 @@
+let canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+let aspectRatio = 16/10;
+let windowedWidth = 1024;
+let windowedHeight = 640;
+canvas.width = windowedWidth;
+canvas.height = windowedHeight;
+
+function update() {
+  if (level == 'game') {
+    ctx.drawImage(background,0,0);
+    //add new tile at begining of each lane
+    if (count % 32 === 0) {
+      for (let i = 0; i < lanes.length; i++) {
+        platform[lanes[i]].push({x:1024,y:laneHeight[i],tilePos:getRandomTile(10),item:null});
+      }
+    }
+    //shift tiles left and draw them
+    for (lane in platform) {
+      platform[lane].forEach(tile => {
+        ctx.drawImage(tiles,tile.tilePos*64,0,64,64,tile.x,tile.y,64,64);
+        if (tile.item != null) {
+          ctx.drawImage(items,tile.item*64,0,64,64,tile.x,tile.y,64,64);
+        }
+        tile.x-=2;
+        if(tile.x < -128) {
+          platform[lane].shift();
+        }
+      });
+    }
+    ctx.drawImage(inserterSlide,0,0);
+    ctx.drawImage(inserterImg,inserter.x,laneHeight[inserter.lane]-16);
+    ctx.drawImage(playerImg,playerFrame*64,0,64,64,player.x,laneHeight[player.lane],64,64);
+    for (let i = 0; i < hand.length; i++) {
+      if (hand[i] != null) {
+        ctx.drawImage(cards,hand[i]*128,0,128,160,160+(192*i),416,128,160);
+      }
+    }
+    count++;
+    ctx.font = "30px Arial";
+    ctx.fillText(Math.floor(count/10),776,48);
+    if (count % 4 === 0) {
+      playerFrame < 5 ? playerFrame++ : playerFrame = 0;
+    }
+    if (platform[lanes[player.lane]][2].item == 0) {
+      player.lane--;
+    }
+    if (platform[lanes[player.lane]][2].item == 1) {
+      player.lane++;
+    }
+    if (player.lane < 0 || player.lane > 3 || platform[lanes[player.lane]][2].tilePos > 15 && (platform[lanes[player.lane]][2].item != 2 && platform[lanes[player.lane]][2].item != 3)) {
+      level = 'end';
+    }
+  }
+  if (level == 'end') {
+    ctx.drawImage(gameOver,288,180);
+    if (waitingKeys.includes(32)) {
+      location.reload();
+    }
+  }
+  requestAnimationFrame(update);
+}
+
+window.addEventListener("load", function() {
+  update();
+});
+
+document.addEventListener("keydown", function(e) {
+  //up
+  if(e.keyCode === 38 && !waitingKeys.includes(38) && inserter.lane > 0) {
+    inserter.lane -= 1;
+  }
+  //down
+  if(e.keyCode === 40 && !waitingKeys.includes(40) && inserter.lane < 3) {
+    inserter.lane += 1;
+  }
+
+  //Q W E R
+  let cardCodes = [81,87,69,82];
+  for ( let i = 0; i < cardCodes.length; i++) {
+    if(e.keyCode === cardCodes[i] && !waitingKeys.includes(cardCodes[i])) {
+      let curLane = platform[lanes[inserter.lane]];
+      for (let j = 0; j < curLane.length; j++) {
+        curTile = curLane[j];
+        if (curTile.x > 288) {
+          if((curTile.tilePos <= 15 || hand[i] > 1) && curTile.item == null) {
+            curTile.item = hand[i];
+            hand[i] = null;
+            setTimeout(()=> {
+              hand[i] = getRandom(0,4);
+            }, 2000);
+          }
+          break;
+        }
+      }
+    }
+  }
+  if(!waitingKeys.includes(e.keyCode)) {
+    waitingKeys.push(e.keyCode);
+  }
+}, false);
+
+document.addEventListener("keyup", function(e) {
+  if(waitingKeys.includes(e.keyCode)) {
+    waitingKeys.splice(waitingKeys.indexOf(e.keyCode),1);
+  }
+}, false);
---
+++ js/config.js
@@ -0,0 +1,53 @@
+let background = new Image(1024, 640);
+background.src = './assets/background.png';
+let tiles = new Image(1024, 64);
+tiles.src = './assets/tiles.png';
+let inserterSlide = new Image(1024, 640);
+inserterSlide.src = './assets/inserterSlide.png';
+let inserterImg = new Image(96, 96);
+inserterImg.src = './assets/inserter.png';
+let playerImg = new Image(384, 64);
+playerImg.src = './assets/player.png';
+let cards = new Image(768, 160);
+cards.src = './assets/cards.png';
+let items = new Image(128, 64);
+items.src = './assets/items.png';
+let gameOver = new Image(448, 200);
+gameOver.src = './assets/gameOver.png';
+let count = 0;
+let playerFrame = 0;
+let lanes = ['a','b','c','d'];
+let laneHeight = [128,192,256,320];
+let waitingKeys = [];
+let level = 'game';
+
+function getRandom(min,max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomTile(hazardPercent) {
+  if ( getRandom(0,100) <= hazardPercent) {
+    return getRandom(16,18);
+  } else {
+    return getRandom(0,16);
+  }
+}
+
+let platform = {
+  a : [],
+  b : [],
+  c : [],
+  d : []
+};
+
+let player = {x:54,lane:getRandom(0,4)};
+
+let hand = [getRandom(0,4),getRandom(0,4),getRandom(0,4),getRandom(0,4)];
+
+let inserter = {x:304,lane:getRandom(0,4)};
+
+for (let i = 0; i < lanes.length; i++) {
+  for (let j = 0; j <= 960; j+=64) {
+    platform[lanes[i]].push({x:j,y:laneHeight[i],tilePos:getRandom(0,16),item:null});
+  }
+}
